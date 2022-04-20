732214db7a2659eaa45b31839c49fdb2ff5e1ac3
Beth Sutton <bksutton2@gmail.com>
2022-04-20 17:27:41 -0600

    get github repos for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
+// const axios = require('axios');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -303,6 +306,39 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   } catch (error) {
     console.log(error.message);
     res.status(500).send('Server error');
+  }
+});
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async (req, res) => {
+  try {
+    const options = {
+      uri: encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      ),
+      method: 'GET',
+      headers: {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`,
+      },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      }
+
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send('Server error');
   }
 });
 
