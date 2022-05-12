6e12671899508ff35c3c9046166fb0e83b803da1
Beth Sutton <bksutton2@gmail.com>
2022-05-12 15:09:55 -0600

    update

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,6 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
-// import NotFound from './components/layout/NotFound';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { LOGOUT } from './actions/types';
 
@@ -27,60 +26,60 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 const App = () => {
-  useEffect(() => {
-    // check for token in LS when app first runs
-    if (localStorage.token) {
-      // if there is a token set axios headers for all requests
-      setAuthToken(localStorage.token);
-    }
-    // try to fetch a user, if no token or invalid token we
-    // will get a 401 response from our API
-    store.dispatch(loadUser());
+	useEffect(() => {
+		// check for token in LS when app first runs
+		if (localStorage.token) {
+			// if there is a token set axios headers for all requests
+			setAuthToken(localStorage.token);
+		}
+		// try to fetch a user, if no token or invalid token we
+		// will get a 401 response from our API
+		store.dispatch(loadUser());
 
-    // log user out from all tabs if they log out in one tab
-    window.addEventListener('storage', () => {
-      if (!localStorage.token) store.dispatch({ type: LOGOUT });
-    });
-  }, []);
+		// log user out from all tabs if they log out in one tab
+		window.addEventListener('storage', () => {
+			if (!localStorage.token) store.dispatch({ type: LOGOUT });
+		});
+	}, []);
 
-  return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Alert />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="profiles" element={<Profiles />} />
-          <Route path="profile/:id" element={<Profile />} />
-          <Route
-            path="dashboard"
-            element={<PrivateRoute component={Dashboard} />}
-          />
-          <Route
-            path="create-profile"
-            element={<PrivateRoute component={ProfileForm} />}
-          />
-          <Route
-            path="edit-profile"
-            element={<PrivateRoute component={ProfileForm} />}
-          />
-          <Route
-            path="add-experience"
-            element={<PrivateRoute component={AddExperience} />}
-          />
-          <Route
-            path="add-education"
-            element={<PrivateRoute component={AddEducation} />}
-          />
-          <Route path="posts" element={<PrivateRoute component={Posts} />} />
-          <Route path="posts/:id" element={<PrivateRoute component={Post} />} />
-          {/* <Route path="/*" element={<NotFound />} /> */}
-        </Routes>
-      </Router>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<Router>
+				<Navbar />
+				<Alert />
+				<Routes>
+					<Route path='/' element={<Landing />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='profiles' element={<Profiles />} />
+					<Route path='profile/:id' element={<Profile />} />
+					<Route
+						path='dashboard'
+						element={<PrivateRoute component={Dashboard} />}
+					/>
+					<Route
+						path='create-profile'
+						element={<PrivateRoute component={ProfileForm} />}
+					/>
+					<Route
+						path='edit-profile'
+						element={<PrivateRoute component={ProfileForm} />}
+					/>
+					<Route
+						path='add-experience'
+						element={<PrivateRoute component={AddExperience} />}
+					/>
+					<Route
+						path='add-education'
+						element={<PrivateRoute component={AddEducation} />}
+					/>
+					<Route path='posts' element={<PrivateRoute component={Posts} />} />
+					<Route path='posts/:id' element={<PrivateRoute component={Post} />} />
+					{/* <Route path="/*" element={<NotFound />} /> */}
+				</Routes>
+			</Router>
+		</Provider>
+	);
 };
 
 export default App;
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -9,36 +9,36 @@ import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading } }) => {
-  const { id } = useParams();
-  useEffect(() => {
-    getPost(id);
-  }, [getPost, id]);
+	const { id } = useParams();
+	useEffect(() => {
+		getPost(id);
+	}, [getPost, id]);
 
-  return loading || post === null ? (
-    <Spinner />
-  ) : (
-    <section className="container">
-      <Link to="/posts" className="btn">
-        Back To Posts
-      </Link>
-      <PostItem post={post} showActions={false} />
-      <CommentForm postId={post._id} />
-      <div className="comments">
-        {post.comments.map((comment) => (
-          <CommentItem key={comment._id} comment={comment} postId={post._id} />
-        ))}
-      </div>
-    </section>
-  );
+	return loading || post === null ? (
+		<Spinner />
+	) : (
+		<section className='container'>
+			<Link to='/posts' className='btn'>
+				Back To Posts
+			</Link>
+			<PostItem post={post} showActions={false} />
+			<CommentForm postId={post._id} />
+			<div className='comments'>
+				{post.comments.map((comment) => (
+					<CommentItem key={comment._id} comment={comment} postId={post._id} />
+				))}
+			</div>
+		</section>
+	);
 };
 
 Post.propTypes = {
-  getPost: PropTypes.func.isRequired,
-  post: PropTypes.object.isRequired,
+	getPost: PropTypes.func.isRequired,
+	post: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  post: state.post,
+	post: state.post,
 });
 
 export default connect(mapStateToProps, { getPost })(Post);
